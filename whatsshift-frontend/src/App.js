ce48d5379e6f16acee69cad10e58511357b127ac
DantasBeyer <dantasbeyer@hotmail.com>
2024-07-23 14:40:32 +0200

    dashboard component created

--- whatsshift-frontend/src/App.js
+++ whatsshift-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./assets/components/Login";
+import Dashboard from "./assets/components/Dashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" exact Component={Login} />
+        <Route path="dashboard" exact Component={Dashboard} />
       </Routes>
     </Router>
   );
