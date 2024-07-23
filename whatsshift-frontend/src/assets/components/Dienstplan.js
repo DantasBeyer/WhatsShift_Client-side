ba6aa7c26833622e0ac5df4205417ac6aeb88a75
DantasBeyer <dantasbeyer@hotmail.com>
2024-07-23 15:35:29 +0200

    updated

---
+++ whatsshift-frontend/src/assets/components/Dienstplan.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dienstplan = () => {
+  return (
+    <div className="min-h.screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-3x1 font-bold mb-4"> Dienstplan</h1>
+    </div>
+  );
+};
+
+export default Dienstplan;
